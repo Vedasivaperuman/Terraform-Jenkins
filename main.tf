96930f794690f78566d209fcd965d5a31f51b9c6
Vedasivaperuman <vedasivaperuman@gmail.com>
2025-03-11 10:56:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "Tf" {
+  ami           = "ami-04b4f1a9cf54c11d0" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "Tf-Instance"
+  }
+}
