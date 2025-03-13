11e4769dfb13d9af493970db47a11b350d8e8ce4
Vedasivaperuman <vedasivaperuman@gmail.com>
2025-03-13 11:01:40 +0530

    Delete jenkins.tf

--- jenkins.tf
+++
@@ -1,8 +0,0 @@
-pipeline {
-
-   parameters {
-       booleanparam(name: 'autoapprove', defaultvalue: false,description: 'Automatically run apply after generating plan?')
-   }
-   environment {
-      AWS_ACCESS_KEY_ID     = credentials('AWS_ACCESS_KEY_ID')
-      AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
