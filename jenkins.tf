c9edee669c9f0689ed34999f841218dea465f466
Vedasivaperuman <vedasivaperuman@gmail.com>
2025-03-13 11:00:33 +0530

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -4,5 +4,5 @@ pipeline {
        booleanparam(name: 'autoapprove', defaultvalue: false,description: 'Automatically run apply after generating plan?')
    }
    environment {
-      AWS_ACCESS_KEY_ID     = credentials ('AWS_ACCESS_KEY_ID')
-      AWS_SECRET_ACCESS_KEY = credentials ('AWS_SECRET_ACCESS_KEY')
+      AWS_ACCESS_KEY_ID     = credentials('AWS_ACCESS_KEY_ID')
+      AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
